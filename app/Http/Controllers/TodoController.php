2217701cd796dcf1fa1fc7b0b0f7e03968e209d3
Luis Ochoa <ochoaal98@gmail.com>
2023-01-20 12:15:00 -0400

    feat(api resources): Add Todo api resource incluiding, model, controller, migration, factory and seeder file

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::paginate(30);
+        // Could use a Resource insted
+        return response()->json($todos);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+}
