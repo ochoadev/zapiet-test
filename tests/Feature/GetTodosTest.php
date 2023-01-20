10142ca55a161406c819505854879cd003ad09e9
Luis Ochoa <ochoaal98@gmail.com>
2023-01-20 11:29:16 -0400

    refactor(tests): Remove un-used class in GetTodosTest

--- tests/Feature/GetTodosTest.php
+++ tests/Feature/GetTodosTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class GetTodosTest extends TestCase
